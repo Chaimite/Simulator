8df40462d5e2804b92cc68575d7069957d6fe27c
Chaimite <adrianocorreiacesar@gmail.com>
2019-07-13 16:44:09 +0100

    improved lane adding

--- application/Controller.java
+++ application/Controller.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.animation.PathTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -23,7 +28,7 @@ public class Controller implements Initializable
    private Circle lane;
 
    @FXML
-   private Circle green_center;
+   private Circle greenCenter;
 
 
    @FXML
@@ -32,21 +37,25 @@ public class Controller implements Initializable
    @FXML
    private Button removeLane;
 
+   private double newLaneRadius = 10;
+   private double centerYCoordinate = greenCenter.getCenterY();
+   private double centerXCoordinate = greenCenter.getCenterX();
+   private double radiusOfLane = lane.getRadius();
    
-   public void addLaneOnClick(ActionEvent event)
+   double innerRadius = lane.getRadius();
+   double outerRadius = radiusOfLane + newLaneRadius;
+   
+   public void addLaneOnClick(ActionEvent event) throws IOException
    {
-      // This method will make the button add a track
-      
+      // This method will make the button add a track    
       // Still needs to have a location(coordinates), and probably the
       // anchor pane
-      System.out.println("Well you pressed the button");
-      double innerRadius = 350;
-      double outerRadius = 360;
+  
       
-      Circle outerCircle = new Circle(outerRadius, Color.rgb(255, 255, 255, 0));
+      Circle outerCircle = new Circle(centerXCoordinate,centerYCoordinate,outerRadius, Color.rgb(255, 255, 255, 0));
       outerCircle.setStroke(Color.WHITE);
       
-      Circle innerCircle = new Circle(innerRadius, Color.rgb(211, 211, 211, 1));
+      Circle innerCircle = new Circle(centerXCoordinate, centerYCoordinate,innerRadius, Color.rgb(211, 211, 211, 1));
       innerCircle.setStroke(Color.BLUE);
       innerCircle.getStrokeDashArray().add(45d);
       
@@ -58,6 +67,19 @@ public class Controller implements Initializable
    // This method will make the button remove a track
    }
 
+   static void vehicle(double radius)
+   {
+      Circle vehiclePath = new Circle(radius);
+      // Transition type for vehicle
+      PathTransition transition = new PathTransition();
+      transition.setDuration(Duration.seconds(5));
+      transition.setPath(vehiclePath);
+      transition.setOrientation(
+            PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+      transition.setCycleCount(PathTransition.INDEFINITE);
+      transition.play();
+
+   }
    @Override
    public void initialize(URL location, ResourceBundle resources)
    {
@@ -85,18 +107,6 @@ public class Controller implements Initializable
 
    }
 
-   static void vehicle(double radius)
-   {
-      Circle vehiclePath = new Circle(radius);
-      // Transition type for vehicle
-      PathTransition transition = new PathTransition();
-      transition.setDuration(Duration.seconds(5));
-      transition.setPath(vehiclePath);
-      transition.setOrientation(
-            PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-      transition.setCycleCount(PathTransition.INDEFINITE);
-      transition.play();
-
-   }
+  
 
 }
