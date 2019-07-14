5f04500f9c789bd6bbb7f0e8032ea9f09ff1859d
Chaimite <adrianocorreiacesar@gmail.com>
2019-07-14 16:10:48 +0100

    Add lane button working

--- application/Controller.java
+++ application/Controller.java
@@ -3,17 +3,14 @@ package application;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Stack;
 
 import javafx.animation.PathTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -28,8 +25,7 @@ public class Controller implements Initializable
    private Circle lane;
 
    @FXML
-   private Circle greenCenter;
-
+   private Circle trackCenter;
 
    @FXML
    private Button addLane;
@@ -37,45 +33,66 @@ public class Controller implements Initializable
    @FXML
    private Button removeLane;
 
-   private double newLaneRadius = 10;
-   private double centerYCoordinate = greenCenter.getCenterY();
-   private double centerXCoordinate = greenCenter.getCenterX();
-   private double radiusOfLane = lane.getRadius();
+   @FXML
+   private AnchorPane track;
    
-   double innerRadius = lane.getRadius();
-   double outerRadius = radiusOfLane + newLaneRadius;
    
+
    public void addLaneOnClick(ActionEvent event) throws IOException
    {
       // This method will make the button add a track    
       // Still needs to have a location(coordinates), and probably the
       // anchor pane
-  
+      // Not sure if I should give it a return different from void
+      double newLaneRadius = 20;
+
+      // Data about coordinates to center objects according with the track
+      // center
+      double centerXCoordinate = trackCenter.getLayoutX();
+      double centerYCoordinate = trackCenter.getLayoutY();
+
+      // Data from initial track
+     double innerRadius = lane.getRadius();
+      double outerRadius = lane.getRadius() + newLaneRadius;
       
-      Circle outerCircle = new Circle(centerXCoordinate,centerYCoordinate,outerRadius, Color.rgb(255, 255, 255, 0));
-      outerCircle.setStroke(Color.WHITE);
+     
       
-      Circle innerCircle = new Circle(centerXCoordinate, centerYCoordinate,innerRadius, Color.rgb(211, 211, 211, 1));
-      innerCircle.setStroke(Color.BLUE);
+      Circle outerCircle = new Circle(centerXCoordinate,centerYCoordinate,outerRadius,Color.TRANSPARENT);
+    
+      outerCircle.setStroke(Color.rgb(211, 211, 211));
+      outerCircle.setStrokeWidth(20);
+      
+      
+      Circle innerCircle = new Circle(centerXCoordinate, centerYCoordinate,innerRadius, Color.TRANSPARENT);
+      innerCircle.setStroke(Color.BLACK);
       innerCircle.getStrokeDashArray().add(45d);
       
+      track.getChildren().addAll(outerCircle,innerCircle);
+      Stack<Circle> test = new Stack<>();
+      for (Circle circle : test)
+      {
+         test.add(circle);         
+      }     
+   }
+
+   public void removeLaneOnClick(ActionEvent event)
+   {
+   // This method will make the button remove a track
+//      track.getChildren().remove();
+      Stack <Circle> test = new Stack<>();
+      
+//     test.pop(circle);
       
    }
 
-   public void removeLaneOnClick()
+    void vehicle(double xCoordinate, double yCoordinate,double radius)
    {
-   // This method will make the button remove a track
-   }
-
-   static void vehicle(double radius)
-   {
-      Circle vehiclePath = new Circle(radius);
-      // Transition type for vehicle
+           // Transition type for vehicle
       PathTransition transition = new PathTransition();
+      transition.setNode(vehicle);
       transition.setDuration(Duration.seconds(5));
-      transition.setPath(vehiclePath);
-      transition.setOrientation(
-            PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+      transition.setPath(this.lane);
+      transition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
       transition.setCycleCount(PathTransition.INDEFINITE);
       transition.play();
 
@@ -83,30 +100,13 @@ public class Controller implements Initializable
    @Override
    public void initialize(URL location, ResourceBundle resources)
    {
-
+      
+      
+      
+      
       // Establish path format and radius
       double radius = 110;
-//      vehicle(radius);
-      Circle circle = new Circle(radius);
-
-      // Establish location of format location
-      // circle.setCenterX(110);
-      // circle.setCenterY(225);
-
-      // Create transition type
-      PathTransition transition = new PathTransition();
-      // Establish what object to follow path
-      transition.setNode(vehicle);
-      //
-      transition.setDuration(Duration.seconds(5));
-      transition.setPath(circle);
-      transition.setOrientation(
-            PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-      transition.setCycleCount(PathTransition.INDEFINITE);
-      transition.play();
+      vehicle(lane.getCenterX(), lane.getCenterY(), radius);
 
    }
-
-  
-
 }
