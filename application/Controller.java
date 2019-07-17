8118f05fb85dc2a71eb436d421ba192d3a221adc
Chaimite <adrianocorreiacesar@gmail.com>
2019-07-17 14:41:17 +0100

    add and remove buttons functionality completed

--- application/Controller.java
+++ application/Controller.java
@@ -8,8 +8,11 @@ import javafx.animation.PathTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -44,7 +47,7 @@ public class Controller implements Initializable
    private Circle outerCircle;
    private double lastElementRadius;
    private Circle lastElementInTrack;
-//   private double elementRadius;
+   private double laneRadius = 22;
    private double newRadius;
    private Stack<Circle> trackStack = new Stack<>();
    private int elementsInTrack;
@@ -53,19 +56,14 @@ public class Controller implements Initializable
    {
       // This method will make the button add a track
       
-      // Data from initial track
+      // Data from the last inserted lane
       lastElementInTrack = trackStack.peek();
       centerXCoordinate = lastElementInTrack.getLayoutX();
-      centerYCoordinate = lastElementInTrack.getLayoutY();
-      
+      centerYCoordinate = lastElementInTrack.getLayoutY();     
       lastElementRadius = lastElementInTrack.getRadius();
       
-      newRadius = lastElementRadius + 22;
-      System.out.println("outer radius: " + lastElementRadius);
-      
-      System.out.println("new radius " + newRadius);
-
-      // The new lane
+      // A new lane
+      newRadius = lastElementRadius + laneRadius;
       outerCircle = new Circle(centerXCoordinate, centerYCoordinate,newRadius, Color.TRANSPARENT);
       outerCircle.setStroke(Color.rgb(211, 211, 211));
       outerCircle.setStrokeWidth(25);
@@ -76,7 +74,11 @@ public class Controller implements Initializable
 //      innerCircle.getStrokeDashArray().add(10d);
 
       // Placing the circle in the stack
-      track.getChildren().addAll(trackStack.push(outerCircle));
+      elementsInTrack = track.getChildren().size();
+      if (elementsInTrack <= 5)
+      {
+         track.getChildren().addAll(trackStack.push(outerCircle));
+      }
    }
 
    public void removeLaneOnClick(ActionEvent event)
@@ -84,7 +86,9 @@ public class Controller implements Initializable
       // This method will make the button remove a track 
       elementsInTrack = track.getChildren().size();
       if (elementsInTrack >= 3)
-         track.getChildren().remove(trackStack.pop());       
+      {
+         track.getChildren().remove(trackStack.pop());
+      }
    }
 
    void path(double xCoordinate, double yCoordinate, double radius)
@@ -106,8 +110,8 @@ public class Controller implements Initializable
       trackStack.push(trackCenter);
       // Establish path format and radius
       double radius = 110;
-      path(trackCenter.getCenterX(), trackCenter.getCenterY(), radius);
-//      vehicleStackPane.setBackground(rgb);
+      path(vehicleStackPane.getLayoutX(), vehicleStackPane.getLayoutY(), radius);
+      vehicleStackPane.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT,CornerRadii.EMPTY, Insets.EMPTY)));
      
 
    }
--- test/testBed.java
+++ test/testBed.java
@@ -5,7 +5,7 @@ public class testBed
 
    public static void main(String[] args)
    {
-      public int upd
+     
 
    }
 
