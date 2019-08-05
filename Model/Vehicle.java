0f705d0a14e9d47aaeea7072074024c74e3fff37
Chaimite <adrianocorreiacesar@gmail.com>
2019-08-05 22:35:32 +0100

    vehicle stopping when distance < 20

--- Model/Vehicle.java
+++ Model/Vehicle.java
@@ -64,87 +64,73 @@ public class Vehicle implements Observer, Runnable
          }
          // when there is an accident on the road, calculate the distance and if the distance is less than the specified then it should pause.
          double distance = calculateDistanceBetweenAccidentAndVehicle();
-         System.out.println(distance);
-         if(distance < 2.0)
+         if (distance < 20.0)
          {
             stopVehicle();
          }
-         
+
+         try
+         {
+            Thread.sleep(100);
+         }
+         catch (InterruptedException e)
+         {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
          // look on the other lane to see if it can move.
-         
-         
       }
-//      while(true)                      
-//      {
-//         
-//         while(!checkCollisionBetweenBlockingElementAndVehicle() && !checkCollisionBetweenVehicles())
-//         {
-//            // do nothing
-//         }
-//         // collision detected coming out of the while loop
-//         // check side collision with rectangle/vehicle, in the y axis for side vehicle
-//         checkCollisionBetweenVehicles(); // for the sides, also need to verify if it has lanes on the side
-//         stopVehicle();
-//         
-//         
-//         while(checkCollisionBetweenBlockingElementAndVehicle())
-//         {
-//            // check shape collision with rectangle/vehicle type
-//            // do nothing
-//            // maybe later here see if the car can change lane.
-//         }
-//      }
-      
-//      while(true)
-//      {
-//         startVehicle();
-//         if(!isFrontBlockDetected()) {
-//            // Need to have this being detect
-//            return;
-//         }
-//         else if(isLaneOnRight) {
-//            if(!isRightSideBlockDetected()) {
-//               
-//            }
-//         }
-//      }
    }
 
    private double calculateDistanceBetweenAccidentAndVehicle()
    {
-//      Point2D v = new Point2D(vehicle.getX(), vehicle.getY());
-//      Point2D be = new Point2D(blockingObject.getCenterX(), blockingObject.getCenterY());
-//      v.distan
-      Bounds boundsInV = vehicle.localToScene(vehicle.getBoundsInLocal());
-      Point2D ff = vehicle.localToScene(vehicle.getX(), vehicle.getY());
-      double vX = getGlobalX(vehicle);
-      double vY = getGlobalY(vehicle);
-      double bX = getGlobalX(blockingObject);
-      double bY = getGlobalY(blockingObject);
       
-      Point2D v = new Point2D(ff.getX(), ff.getX());
-      System.out.println("vehicle x " + ff.getX() + " vehicle y " + ff.getY());
-      Point2D b = new Point2D(blockingObject.getCenterX(), blockingObject.getCenterY());
+      Bounds vB = vehicle.getBoundsInParent();
+      double vCenterX = (vB.getMinX() + vB.getMaxX()) / 2.0;
+      System.out.println("vehicle Minx " + vB.getMinX() + " vehicle Miny " + vB.getMinY());
+      System.out.println("vehicle MaxX " + vB.getMaxX() + " vehicle MaxY " + vB.getMaxY());
       
-      return v.distance(b);
+      Bounds bB = blockingObject.getBoundsInParent();
+      double bCenterX = (bB.getMinX() + bB.getMaxX()) / 2.0;
+      double bCenterY = (bB.getMinY() + bB.getMaxY()) / 2.0;
       
+      System.out.println("Block Minx " + bB.getMinX() + " block Miny " + bB.getMinY());
+      System.out.println("Block MaxX " + bB.getMaxX() + " Block MaxY " + bB.getMaxY());
+      System.out.println("Block CenterX " + bCenterX + " Block CenterY " + bCenterY);
+      
+      
+      Point2D v = new Point2D(vCenterX, vB.getMaxY());
+      Point2D b = new Point2D(bCenterX, bCenterY);
+      
+      double distance =  v.distance(b);
+      
+      
+      System.out.println("distance : " + distance);
+      System.out.println();
+      System.out.println("---------------------------------------------------------");
+      System.out.println();
+      
+      return distance;
+
    }
    
-   private double getGlobalX(Node node) {
-      if (node == null) {
-          return 0.0;
-      }
-      double parentGlobalX = getGlobalX(node.getParent());
-      return node.getLayoutX() - parentGlobalX;
-  }
-
-  private double getGlobalY(Node node) {
-      if (node == null) {
-          return 0.0;
-      }
-      double parentGlobalY = getGlobalY(node.getParent());
-      return parentGlobalY - node.getLayoutY();
-  }
+   
+//   
+//   private double getGlobalX(Node node) {
+//      if (node == null) {
+//          return 0.0;
+//      }
+//      double parentGlobalX = getGlobalX(node.getParent());
+//      return node.getLayoutX() - parentGlobalX;
+//  }
+//
+//  private double getGlobalY(Node node) {
+//      if (node == null) {
+//          return 0.0;
+//      }
+//      double parentGlobalY = getGlobalY(node.getParent());
+//      return parentGlobalY - node.getLayoutY();
+//  }
 
    public void setupVehicle(Rectangle vehicle, Circle baseCarriageway)
    {
